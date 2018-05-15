10f31204b2608290f7fc0264cf5634ed833ce5b4
smuchka <smuchka.cherry@gmail.com>
2018-05-15 15:33:06 +0300

    prepare footer

---
+++ components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="text-muted">
+        <div class="container">
+            <p class="float-right">
+                <a href="https://github.com/smuchka" target="_blank"><img class="logo-github" src="~/assets/images/github_128.png" alt=""></a>
+            </p>
+            <p>© 2018 - Powered by <a href="https://www.linkedin.com/in/romanchereshnia/">Roman Chereshnia</a></p>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.logo-github {
+  height: 22px;
+}
+</style>
