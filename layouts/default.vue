e1b08e52c47309a391d22d3f79b7ddd02fe0c9a8
smuchka <smuchka.cherry@gmail.com>
2018-05-15 15:34:36 +0300

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <site-header></site-header>
+        <main class="container">
+            <nuxt/>
+        </main>
+        <site-footer/>
+    </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+export default {
+  components: {
+    SiteHeader: () => import("~/components/header"),
+    SiteFooter: () => import("~/components/footer")
+  }
+};
+</script>
+
+<style>
+main.container {
+  min-height: 600px;
+}
+
+main.container h1,
+main.container h3 {
+  padding-top: 25px;
+  padding-bottom: 15px;
+}
+</style>
