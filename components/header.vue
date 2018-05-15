df61b44d083b17ede6dfcfc5ccb2554a7b7281f7
smuchka <smuchka.cherry@gmail.com>
2018-05-15 15:38:31 +0300

    craete header & and welcome page

---
+++ components/header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <div class="navbar navbar-dark bg-dark box-shadow">
+            <div class="container d-flex justify-content-between">
+                <nuxt-link to="/" class="navbar-brand d-flex align-items-center">
+                    <i class="material-icons mr-2">featured_play_list</i>
+                    <strong>PLAYER TOURNAMENT 2018</strong>
+                </nuxt-link>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.navbar-brand > i {
+  max-width: 24px;
+  overflow: hidden;
+}
+</style>
---
+++ pages/welcome.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <h1 class="text-black-50">Interview task of 05.2018</h1>
+    <p>Detailed description of the test task can be seen <a href="/task.pdf">here</a>.</p>
+    <p><nuxt-link :to="{ name: 'tournament-level'}" class="btn btn-primary my-2">Go to implementation</nuxt-link></p>
+  </div>
+</template>
